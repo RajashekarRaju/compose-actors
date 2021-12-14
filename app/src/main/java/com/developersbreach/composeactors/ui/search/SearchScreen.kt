4038d64c7d6d31dc0c4629d23281d9f546067987
RajashekarRaju <rajashekarraju123@gmail.com>
2021-12-15 00:06:07 +0530

    Enabled search functionality by query with SearchViewModel. Code clean-up.

--- app/src/main/java/com/developersbreach/composeactors/ui/search/SearchScreen.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/search/SearchScreen.kt
@@ -1,45 +1,130 @@
 package com.developersbreach.composeactors.ui.search
 
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.TopAppBar
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.developersbreach.composeactors.ui.actors.ActorsViewModel
-import com.developersbreach.composeactors.ui.actors.SearchBar
 import com.developersbreach.composeactors.ui.theme.ComposeActorsTheme
+import com.google.accompanist.insets.systemBarsPadding
 
+
+@Composable
+fun CategoryAppBar(
+    navigateUp: () -> Unit,
+    titles: String
+) {
+    TopAppBar(
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            IconButton(onClick = navigateUp) {
+                Icon(
+                    imageVector = Icons.Rounded.ArrowBack,
+                    tint = MaterialTheme.colors.onBackground,
+                    contentDescription = null
+                )
+            }
+
+            Text(
+                text = titles,
+                color = MaterialTheme.colors.onBackground,
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@SuppressLint("NewApi")
+@Composable
+fun SearchScreen(
+    selectedActor: (Int) -> Unit,
+    navigateUp: () -> Unit,
+    viewModel: SearchViewModel
+) {
+
+    val searchableActors by viewModel.searchUiState.observeAsState(listOf())
+
+    Surface(
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .systemBarsPadding()
+                .padding(horizontal = 8.dp)
+        ) {
+            CategoryAppBar(
+                navigateUp = navigateUp,
+                "Robert Downey Jr"
+            )
+        }
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(searchableActors) { actor ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = actor.actorName,
+                        style = MaterialTheme.typography.h6,
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(24.dp)
+                            .clickable(onClick = { selectedActor(actor.actorId) })
+                    )
+                }
+            }
+        }
+    }
+}
 
 @Preview(widthDp = 360, heightDp = 640)
 @Composable
 fun SearchScreenPreview() {
     ComposeActorsTheme(darkTheme = true) {
-        SearchScreen({ }, viewModel())
-    }
-}
 
-@Composable
-fun SearchScreen(
-    selectedActor: (Int) -> Unit,
-    viewModel: ActorsViewModel
-) {
-    Surface(
-        color = MaterialTheme.colors.background
-    ) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    { SearchBar() },
-                    backgroundColor = MaterialTheme.colors.background,
-                    elevation = 0.dp
+        Surface(
+            color = MaterialTheme.colors.background
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .systemBarsPadding()
+                    .padding(horizontal = 8.dp)
+            ) {
+                CategoryAppBar(
+                    navigateUp = { },
+                    "Robert Downey Jr"
                 )
             }
-        ) {
-
         }
     }
 }
--- app/src/main/java/com/developersbreach/composeactors/utils/NetworkManager.kt
+++ app/src/main/java/com/developersbreach/composeactors/utils/NetworkManager.kt
@@ -5,8 +5,6 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 
 class NetworkManager(
     context: Context
@@ -14,22 +12,6 @@ class NetworkManager(
 
     private val manager: ConnectivityManager? =
         ContextCompat.getSystemService(context, ConnectivityManager::class.java)
-
-    private val _isConnected = MutableLiveData<Boolean>()
-    val isConnected: LiveData<Boolean>
-        get() = _isConnected
-
-    init {
-        manager?.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                _isConnected.postValue(true)
-            }
-
-            override fun onLost(network: Network) {
-                _isConnected.postValue(false)
-            }
-        })
-    }
 
     fun checkForActiveNetwork(): Boolean {
         val activeNetwork: Network? = manager?.activeNetwork
