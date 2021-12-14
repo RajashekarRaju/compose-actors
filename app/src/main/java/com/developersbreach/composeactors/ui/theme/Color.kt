99649e7aa6844b5fb2bc7662e7b7cca831ed7554
RajashekarRaju <rajashekarraju123@gmail.com>
2021-12-14 23:59:35 +0530

    Made changes to app themes and colors. Cleaned up resources.

--- app/src/main/java/com/developersbreach/composeactors/ui/theme/Color.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/theme/Color.kt
@@ -2,11 +2,20 @@ package com.developersbreach.composeactors.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val white = Color(0xFFffffff)
-val grey100 = Color(0xFFf5f5f5)
-val grey300 = Color(0xFFe0e0e0)
-val grey500 = Color(0xFF9e9e9e)
-val grey700 = Color(0xFF616161)
-val grey900 = Color(0xFF212121)
-val grey = Color(0xFF181818)
-val black = Color(0xFF000000)
+val primary = Color(0xFF236D8F)
+val onPrimary = Color(0xFFffffff)
+
+val surface = Color(0xFF1D262E)
+val onSurface = Color(0xFFE3F2FD)
+
+val background = Color(0xFF141A20)
+val onBackground = Color(0xFFE3F2FD)
+
+//val primary = Color(0xFF236D8F)
+//val onPrimary = Color(0xFFffffff)
+//
+//val surface = Color(0xFF1D262E)
+//val onSurface = Color(0xFFE3F2FD)
+//
+//val background = Color(0xFF141A20)
+//val onBackground = Color(0xFFE3F2FD)
--- app/src/main/java/com/developersbreach/composeactors/ui/theme/Theme.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/theme/Theme.kt
@@ -7,23 +7,21 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = grey500,
-    primaryVariant = grey700,
-    secondary = grey500,
-    secondaryVariant = grey300,
-    background = black,
-    onBackground = grey100,
-    surface = grey
+    primary = primary,
+    onPrimary = onPrimary,
+    background = background,
+    onBackground = onBackground,
+    surface = surface,
+    onSurface = onSurface
 )
 
 private val LightColorPalette = lightColors(
-    primary = grey700,
-    primaryVariant = grey500,
-    secondary = grey700,
-    secondaryVariant = grey900,
-    background = white,
-    onBackground = grey900,
-    surface = grey100
+    primary = primary,
+    onPrimary = onPrimary,
+    background = background,
+    onBackground = onBackground,
+    surface = surface,
+    onSurface = onSurface
 )
 
 @Composable
