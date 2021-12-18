8b05e6504162b6b68b87ffe5ec40fdaf90f388e9
RajashekarRaju <rajashekarraju123@gmail.com>
2021-12-18 23:55:40 +0530

    Made typography changes, added content descriptions, updated resources

--- app/src/main/java/com/developersbreach/composeactors/ui/theme/Type.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/theme/Type.kt
@@ -2,27 +2,56 @@ package com.developersbreach.composeactors.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.developersbreach.composeactors.R
 
 // Set of Material typography styles to start with
+private val MontserratAlternates = FontFamily(
+    Font(R.font.montserrat_alternates_light, FontWeight.Light),
+    Font(R.font.montserrat_alternates_regular, FontWeight.Normal),
+    Font(R.font.montserrat_alternates_medium, FontWeight.Medium),
+    Font(R.font.montserrat_alternates_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_alternates_bold, FontWeight.Bold),
+)
+
+// Material typography styles
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+
+    h3 = TextStyle(
+        fontFamily = MontserratAlternates,
+        fontWeight = FontWeight.W800,
+        fontSize = 32.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = MontserratAlternates,
+        fontWeight = FontWeight.W700,
+        fontSize = 32.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = MontserratAlternates,
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = MontserratAlternates,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = MontserratAlternates,
+        fontWeight = FontWeight.W600,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = MontserratAlternates,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    body2 = TextStyle(
+        fontFamily = MontserratAlternates,
+        fontSize = 14.sp
+    ),
 )
