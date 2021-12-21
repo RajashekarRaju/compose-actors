c982e0c68fe8e006c827f45980e1f240746d630c
RajashekarRaju <rajashekarraju123@gmail.com>
2021-12-21 23:48:18 +0530

    Removed original api key, modified gitignore file changes

---
+++ app/src/main/java/com/developersbreach/composeactors/utils/ApiKey.kt
@@ -0,0 +1,10 @@
+package com.developersbreach.composeactors.utils
+
+/**
+ * Add your own TMDB_API key here.
+ *
+ * You can get your own Api Key from here [https://www.themoviedb.org/settings/api]
+ */
+object ApiKey {
+    const val API_KEY = ""
+}
