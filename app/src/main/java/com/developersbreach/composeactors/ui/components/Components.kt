5ff20c4801d8e75b7f31de25739cf5095b101065
RajashekarRaju <rajashekarraju123@gmail.com>
2021-12-29 23:58:27 +0530

    Added reusable composables, improved the way of snackbar handling with host.

--- app/src/main/java/com/developersbreach/composeactors/ui/components/Components.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/components/Components.kt
@@ -8,35 +8,66 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.developersbreach.composeactors.R
+import com.developersbreach.composeactors.utils.ApiKey
 import com.developersbreach.composeactors.utils.NetworkManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 /**
- * Perform network check and show snackbar if offline.
- *
  * @param scaffoldState attach snackbar host state to the scaffold
  * @param scope Remember state of scaffold to manage snackbar
+ * @param snackBarMessage message visible to user
+ */
+@Composable
+private fun LaunchSnackBar(
+    scaffoldState: ScaffoldState,
+    snackBarMessage: String,
+    scope: CoroutineScope = rememberCoroutineScope()
+) {
+    LaunchedEffect(scope) {
+        scope.launch {
+            scaffoldState.snackbarHostState.showSnackbar(
+                message = snackBarMessage,
+                duration = SnackbarDuration.Indefinite
+            )
+        }
+    }
+}
+
+/**
+ * Perform network check and show snackbar if offline.
  */
 @Composable
 fun IfOfflineShowSnackbar(
     scaffoldState: ScaffoldState,
-    context: Context,
-    scope: CoroutineScope = rememberCoroutineScope()
+    context: Context = LocalContext.current
 ) {
     val isOnline = NetworkManager(context).checkForActiveNetwork()
     if (!isOnline) {
-        LaunchedEffect(scope) {
-            scope.launch {
-                scaffoldState.snackbarHostState.showSnackbar(
-                    message = context.getString(R.string.offline_snackbar_message),
-                    duration = SnackbarDuration.Indefinite
-                )
-            }
-        }
+        LaunchSnackBar(
+            scaffoldState,
+            context.getString(R.string.offline_snackbar_message)
+        )
+    }
+}
+
+/**
+ * Show snackbar if tmdb api key is missing from [ApiKey.API_KEY].
+ */
+@Composable
+fun ApiKeyMissingShowSnackbar(
+    scaffoldState: ScaffoldState,
+    context: Context = LocalContext.current
+) {
+    if (ApiKey.API_KEY.isEmpty()) {
+        LaunchSnackBar(
+            scaffoldState,
+            context.getString(R.string.missing_api_key_snackbar_message)
+        )
     }
 }
 
