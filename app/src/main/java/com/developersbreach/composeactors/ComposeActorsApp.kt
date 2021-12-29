5a2dc74ad19bed242c5c52357cb68f89d895945f
RajashekarRaju <rajashekarraju123@gmail.com>
2021-12-29 23:56:29 +0530

    Passed source to repository directly in application class. Renamed objects.

--- app/src/main/java/com/developersbreach/composeactors/ComposeActorsApp.kt
+++ app/src/main/java/com/developersbreach/composeactors/ComposeActorsApp.kt
@@ -1,22 +1,26 @@
 package com.developersbreach.composeactors
 
 import android.app.Application
+import com.developersbreach.composeactors.data.NetworkDataSource
 import com.developersbreach.composeactors.navigation.AppNavigation
 import com.developersbreach.composeactors.repository.AppRepository
 import timber.log.Timber
 
+/**
+ * Only place where the repository has been initialized and passed across all viewModels.
+ * This instance property is used in [AppNavigation] class for all ViewModels to access app data.
+ */
 class ComposeActorsApp : Application() {
 
-    /**
-     * Only place where the repository has been initialized and used in app.
-     * This property is used in [AppNavigation] for all ViewModels to access app data.
-     */
+    // Only place where the repository has been initialized and passed across all viewModels
     lateinit var repository: AppRepository
 
     override fun onCreate() {
         super.onCreate()
 
-        repository = AppRepository()
+        // This is the only data source for whole app.
+        val networkDataSource = NetworkDataSource()
+        repository = AppRepository(networkDataSource)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
