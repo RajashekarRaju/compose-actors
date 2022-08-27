f0188abbb5bd70824e16607170e8df402ac7023a
Raj <rajashekarraju123@gmail.com>
2022-08-28 03:17:18 +0530

    Renamed database name to AppDatabase from FavoritesDatabase.kt, because favorites can be an entity.

--- app/src/main/java/com/developersbreach/composeactors/data/database/AppDatabase.kt
+++ app/src/main/java/com/developersbreach/composeactors/data/database/AppDatabase.kt
@@ -1,13 +1,13 @@
-package com.developersbreach.composeactors.repository.database
+package com.developersbreach.composeactors.data.database
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.developersbreach.composeactors.repository.database.dao.FavoriteActorsDao
-import com.developersbreach.composeactors.repository.database.dao.FavoriteMoviesDao
-import com.developersbreach.composeactors.repository.database.entity.FavoriteActorsEntity
-import com.developersbreach.composeactors.repository.database.entity.FavoriteMoviesEntity
+import com.developersbreach.composeactors.data.database.dao.FavoriteActorsDao
+import com.developersbreach.composeactors.data.database.dao.FavoriteMoviesDao
+import com.developersbreach.composeactors.data.database.entity.FavoriteActorsEntity
+import com.developersbreach.composeactors.data.database.entity.FavoriteMoviesEntity
 
 private const val DATABASE_NAME = "favorites database"
 
@@ -16,17 +16,17 @@ private const val DATABASE_NAME = "favorites database"
     version = 1,
     exportSchema = false
 )
-abstract class FavoritesDatabase : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract val favoriteActorsDao: FavoriteActorsDao
     abstract val favoriteMoviesDao: FavoriteMoviesDao
 }
 
 fun getDatabaseInstance(
     context: Context
-): FavoritesDatabase {
+): AppDatabase {
     return Room.databaseBuilder(
         context.applicationContext,
-        FavoritesDatabase::class.java,
+        AppDatabase::class.java,
         DATABASE_NAME
     )
         .fallbackToDestructiveMigration()
