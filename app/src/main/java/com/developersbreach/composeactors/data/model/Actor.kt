eee18e4b7ff1698ad0d2e6a561bc44d2aeb4fe72
Raj <rajashekarraju123@gmail.com>
2022-08-29 01:53:40 +0530

    Removed file object convertors and moved all those mappers to related models

--- app/src/main/java/com/developersbreach/composeactors/data/model/Actor.kt
+++ app/src/main/java/com/developersbreach/composeactors/data/model/Actor.kt
@@ -2,6 +2,7 @@ package com.developersbreach.composeactors.data.model
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import com.developersbreach.composeactors.data.database.entity.FavoriteActorsEntity
 
 @Immutable
 data class Actor(
@@ -9,3 +10,21 @@ data class Actor(
     val actorName: String,
     val profileUrl: String
 )
+
+fun Actor.actorAsDatabaseModel(): FavoriteActorsEntity {
+    return FavoriteActorsEntity(
+        actorId = this.actorId,
+        actorName = this.actorName,
+        actorProfileUrl = this.profileUrl
+    )
+}
+
+fun List<FavoriteActorsEntity>.actorAsDomainModel(): List<Actor> {
+    return map {
+        Actor(
+            actorId = it.actorId,
+            actorName = it.actorName,
+            profileUrl = it.actorProfileUrl
+        )
+    }
+}
--- app/src/main/java/com/developersbreach/composeactors/data/model/Movie.kt
+++ app/src/main/java/com/developersbreach/composeactors/data/model/Movie.kt
@@ -2,9 +2,26 @@ package com.developersbreach.composeactors.data.model
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import com.developersbreach.composeactors.data.database.entity.FavoriteMoviesEntity
 
 @Immutable
 data class Movie(
     @Stable val movieId: Int,
     val posterPathUrl: String
 )
+
+fun Movie.movieAsDatabaseModel(): FavoriteMoviesEntity {
+    return FavoriteMoviesEntity(
+        movieId = this.movieId,
+        moviePosterUrl = this.posterPathUrl,
+    )
+}
+
+fun List<FavoriteMoviesEntity>.movieAsDomainModel(): List<Movie> {
+    return map {
+        Movie(
+            movieId = it.movieId,
+            posterPathUrl = it.moviePosterUrl
+        )
+    }
+}
