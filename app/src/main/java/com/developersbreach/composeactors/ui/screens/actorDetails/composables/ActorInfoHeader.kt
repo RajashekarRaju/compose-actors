6f9bfd67287a2a37963197e8ae782620071002bc
Raj <rajashekarraju123@gmail.com>
2022-09-17 23:56:56 +0530

    Added preview to composable

--- app/src/main/java/com/developersbreach/composeactors/ui/screens/actorDetails/composables/ActorInfoHeader.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/screens/actorDetails/composables/ActorInfoHeader.kt
@@ -11,10 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.developersbreach.composeactors.R
 import com.developersbreach.composeactors.data.model.ActorDetail
 import com.developersbreach.composeactors.ui.animations.borderRevealAnimation
+import com.developersbreach.composeactors.ui.theme.ComposeActorsTheme
 import com.developersbreach.composeactors.utils.calculateAge
 import com.developersbreach.composeactors.utils.getPlaceOfBirth
 import com.developersbreach.composeactors.utils.getPopularity
@@ -140,4 +142,21 @@ private fun ActorInfoHeaderSubtitle(
         modifier = Modifier.padding(vertical = 8.dp),
         color = MaterialTheme.colors.onBackground
     )
+}
+
+@Preview
+@Composable
+private fun ActorInfoHeaderPreview() {
+    ComposeActorsTheme {
+        ActorInfoHeader(
+            actorData = ActorDetail(
+                actorName = "Kate WinsletKate Winslet",
+                profileUrl = "",
+                biography = "Kate Elizabeth Winslet CBE born 5 October 1975 is an English actress. Known for her work in independent films, particularly period dramas, and for her portrayals of headstrong and complicated women, she has received numerous accolades, including an Academy Award, a Grammy Award, two Primetime Emmy Awards, three BAFTA Awards, and five Golden Globe Awards. Time magazine named Winslet one of the 100 most influential people in the world in 2009 and 2021. She was appointed Commander of the Order of the British Empire (CBE) in 2012.",
+                dateOfBirth = "47",
+                placeOfBirth = "UK",
+                popularity = 52.0
+            )
+        )
+    }
 }
--- app/src/main/java/com/developersbreach/composeactors/ui/screens/actorDetails/composables/ActorRoundProfile.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/screens/actorDetails/composables/ActorRoundProfile.kt
@@ -10,10 +10,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.developersbreach.composeactors.R
 import com.developersbreach.composeactors.ui.components.LoadNetworkImage
+import com.developersbreach.composeactors.ui.theme.ComposeActorsTheme
 
 /**
  * Load circular network image of actor at the top of screen.
@@ -21,7 +23,8 @@ import com.developersbreach.composeactors.ui.components.LoadNetworkImage
 @Composable
 internal fun ActorRoundProfile(
     profileUrl: String,
-    size: Dp = 120.dp
+    size: Dp = 120.dp,
+    profilePreview: Int = R.drawable.adele
 ) {
     Box(
         modifier = Modifier.fillMaxWidth(),
@@ -40,4 +43,10 @@ internal fun ActorRoundProfile(
                 )
         )
     }
+}
+
+@Preview
+@Composable
+fun ActorRoundProfilePreview() = ComposeActorsTheme {
+    
 }
