03224bd2cb877116b0316655388fdea591a68ac0
Raj <rajashekarraju123@gmail.com>
2023-03-11 23:55:30 +0530

    Setup project dependencies, resources for initial testing, write simple test.

---
+++ app/src/androidTest/java/com/developersbreach/composeactors/ui/screens/home/HomeScreenUITest.kt
@@ -0,0 +1,65 @@
+package com.developersbreach.composeactors.ui.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.developersbreach.composeactors.data.model.Movie
+import com.developersbreach.composeactors.ui.screens.search.SearchType
+import kotlinx.coroutines.Job
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class HomeScreenUITest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Composable
+    private fun HomeScreenUIContent(
+        selectedActor: (actorId: Int) -> Unit = { },
+        selectedMovie: (movieId: Int) -> Unit = { },
+        openHomeBottomSheet: () -> Job = { Job() },
+        favoriteMovies: List<Movie> = emptyList(),
+        updateSearchType: (searchType: SearchType) -> Unit = {  },
+        homeSheetUIState: HomeSheetUIState = HomeSheetUIState(),
+        homeUIState: HomeUIState = HomeUIState()
+    ) {
+        HomeScreenUI(
+            selectedActor = selectedActor,
+            selectedMovie = selectedMovie,
+            openHomeBottomSheet = openHomeBottomSheet,
+            homeSheetUIState = homeSheetUIState,
+            favoriteMovies = favoriteMovies,
+            updateSearchType = updateSearchType,
+            homeUIState = homeUIState,
+        )
+    }
+
+    @Test
+    fun assertHomeTabsAreDisplayedAndClickable() {
+        composeTestRule.setContent {
+            HomeScreenUIContent()
+        }
+
+        composeTestRule
+            .onNodeWithText(text = "Actors")
+            .assertIsDisplayed()
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText(text = "Movies")
+            .assertIsDisplayed()
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText(text = "Favorite")
+            .assertIsDisplayed()
+            .performClick()
+    }
+}
