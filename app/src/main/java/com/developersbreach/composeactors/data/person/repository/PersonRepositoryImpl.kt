003dc413f7814ef2d319b887e65e1b1679a737e1
Raj <rajashekarraju123@gmail.com>
2025-01-07 23:01:36 +0530

    Implement simple cache mechanism with LruCache

--- app/src/main/java/com/developersbreach/composeactors/data/person/repository/PersonRepositoryImpl.kt
+++ app/src/main/java/com/developersbreach/composeactors/data/person/repository/PersonRepositoryImpl.kt
@@ -2,6 +2,9 @@ package com.developersbreach.composeactors.data.person.repository
 
 import androidx.lifecycle.LiveData
 import arrow.core.Either
+import com.developersbreach.composeactors.core.cache.CacheKey
+import com.developersbreach.composeactors.core.cache.CacheManager
+import com.developersbreach.composeactors.core.database.entity.toPersonDetail
 import com.developersbreach.composeactors.data.datasource.database.DatabaseDataSource
 import com.developersbreach.composeactors.data.person.model.Person
 import com.developersbreach.composeactors.data.person.model.PersonDetail
@@ -16,7 +19,8 @@ import kotlinx.coroutines.withContext
 @Singleton
 class PersonRepositoryImpl @Inject constructor(
     private val personApi: PersonApi,
-    private val databaseDataSource: DatabaseDataSource
+    private val databaseDataSource: DatabaseDataSource,
+    private val cacheManager: CacheManager,
 ) : PersonRepository {
 
     override suspend fun getPopularPersons(): Either<Throwable, List<Person>> = withContext(Dispatchers.IO) {
@@ -34,7 +38,25 @@ class PersonRepositoryImpl @Inject constructor(
     override suspend fun getPersonDetails(
         personId: Int
     ): Either<Throwable, PersonDetail> = withContext(Dispatchers.IO) {
-        personApi.getPersonDetails(personId)
+        val cacheKey = CacheKey.forPerson(personId)
+        val cachedData = cacheManager.get<PersonDetail>(cacheKey)
+        if (cachedData != null) {
+            return@withContext Either.Right(cachedData)
+        }
+
+        val dbData = databaseDataSource.getPersonDetail(personId)
+        dbData.map {
+            if (it != null) {
+                cacheManager.put(cacheKey, it.toPersonDetail())
+                return@withContext Either.Right(it.toPersonDetail())
+            }
+        }
+
+        personApi.getPersonDetails(personId).map {
+            cacheManager.put(cacheKey, it)
+            databaseDataSource.addPersonDetail(it)
+            it
+        }
     }
 
     override suspend fun getCastDetails(
