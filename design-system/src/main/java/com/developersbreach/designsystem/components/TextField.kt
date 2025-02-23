3c346228367f0e7b07a206ece472432c28857b0d
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-02-23 10:32:35 +0530

    (updated)refactor: wrapped TextField in custom components - CaTextField

--- design-system/src/main/java/com/developersbreach/designsystem/components/TextField.kt
+++ design-system/src/main/java/com/developersbreach/designsystem/components/TextField.kt
@@ -1,40 +1,64 @@
 package com.developersbreach.designsystem.components
 
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+
+data class CaTextFieldLeadingIcon(
+    val modifier: Modifier,
+    val iconModifier: Modifier,
+    val onClick: () -> Unit,
+    val imageVector: ImageVector,
+    val tint: Color,
+    val contentDescription: String?
+)
 
 @Composable
 fun CaTextField(
     modifier: Modifier,
     value: String,
     onValueChange: (String) -> Unit,
-    leadingIcon: @Composable (() -> Unit)? = null,
+    leadingIconInfo: CaTextFieldLeadingIcon,
     trailingIcon: @Composable (() -> Unit)? = null,
-    placeholder: @Composable (() -> Unit)? = null,
-    textStyle: TextStyle = LocalTextStyle.current,
-    singleLine: Boolean = false,
-    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
-    minLines: Int = 1,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    colors: TextFieldColors = TextFieldDefaults.textFieldColors()
-    ) {
+    placeholderText: String? = null,
+    textStyle: TextStyle = MaterialTheme.typography.subtitle1,
+    singleLine: Boolean = true,
+    maxLines: Int = 1,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(
+        keyboardType = KeyboardType.Text
+    ),
+    colors: TextFieldColors = TextFieldDefaults.textFieldColors(
+        backgroundColor = Color.Transparent
+    )
+){
     TextField(
         modifier = modifier,
         value = value,
         onValueChange = onValueChange,
-        leadingIcon = leadingIcon,
+        leadingIcon = {
+            CaIconButton(
+                modifier = leadingIconInfo.modifier,
+                iconModifier = leadingIconInfo.iconModifier,
+                onClick = {leadingIconInfo.onClick()},
+                imageVector = leadingIconInfo.imageVector,
+                tint = leadingIconInfo.tint,
+                contentDescription = leadingIconInfo.contentDescription
+            )
+        },
         trailingIcon = trailingIcon,
-        placeholder = placeholder,
+        placeholder = { Text(text=placeholderText ?: "") },
         textStyle = textStyle,
         singleLine = singleLine,
         maxLines = maxLines,
-        minLines = minLines,
         keyboardOptions = keyboardOptions,
         colors = colors
     )
