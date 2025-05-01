b8b89e4503ad5b1ea3b891ef46aed9155c100cec
Raj <rajashekarraju123@gmail.com>
2025-05-01 18:00:16 +0530

    Extract session logic to GetSessionState use-case

--- app/src/main/java/com/developersbreach/composeactors/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/developersbreach/composeactors/ui/screens/splash/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.developersbreach.composeactors.ui.screens.splash
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.developersbreach.composeactors.domain.session.SessionState
 import com.developersbreach.composeactors.ui.components.UiStateHandler
 
 @Composable
