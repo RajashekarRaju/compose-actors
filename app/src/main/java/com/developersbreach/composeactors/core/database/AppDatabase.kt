b5d99f24db58d547db994d710a7b19cd7f997470
Raj <rajashekarraju123@gmail.com>
2025-07-08 19:25:57 +0530

    Clean slate with Room database versioning for production steps

--- app/src/main/java/com/developersbreach/composeactors/core/database/AppDatabase.kt
+++ app/src/main/java/com/developersbreach/composeactors/core/database/AppDatabase.kt
@@ -2,8 +2,6 @@ package com.developersbreach.composeactors.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.developersbreach.composeactors.core.database.dao.PersonDetailsDao
 import com.developersbreach.composeactors.core.database.dao.SessionsDao
 import com.developersbreach.composeactors.data.watchlist.cache.WatchlistPersonsDao
@@ -26,7 +24,7 @@ import com.developersbreach.composeactors.data.watchlist.cache.WatchlistPersonRe
         WatchlistMoviesRemoteKeysEntity::class,
         WatchlistPersonRemoteKeysEntity::class,
     ],
-    version = 10,
+    version = 1,
     exportSchema = true,
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -36,53 +34,4 @@ abstract class AppDatabase : RoomDatabase() {
     abstract val sessionsDao: SessionsDao
     abstract val watchlistMoviesRemoteKeysDao: WatchlistMoviesRemoteKeysDao
     abstract val watchlistPeopleRemoteKeysDao: WatchlistPeopleRemoteKeysDao
-
-    companion object {
-
-        const val OLD_MOVIES_TABLE = "favorite_movies_table"
-        const val NEW_MOVIES_TABLE = "watchlist_movies_table"
-
-        const val OLD_PERSONS_TABLE = "favorite_persons_table"
-        const val NEW_PERSONS_TABLE = "watchlist_persons_table"
-
-        val MIGRATION_7_8 = object : Migration(7, 8) {
-            override fun migrate(
-                db: SupportSQLiteDatabase,
-            ) {
-                db.execSQL(
-                    """
-                    CREATE TABLE IF NOT EXISTS `watchlist_movie_remote_keys_table` (
-                        `movieId` INTEGER NOT NULL PRIMARY KEY,
-                        `prevKey`     INTEGER,
-                        `nextKey`     INTEGER
-                    )
-                    """.trimIndent(),
-                )
-            }
-        }
-
-        val MIGRATION_6_7 = object : Migration(6, 7) {
-            override fun migrate(
-                db: SupportSQLiteDatabase,
-            ) {
-                db.execSQL("ALTER TABLE `$OLD_MOVIES_TABLE` RENAME TO `$NEW_MOVIES_TABLE`")
-                db.execSQL("ALTER TABLE `$OLD_PERSONS_TABLE` RENAME TO `$NEW_PERSONS_TABLE`")
-            }
-        }
-
-        val MIGRATION_5_6 = object : Migration(5, 6) {
-            override fun migrate(
-                db: SupportSQLiteDatabase,
-            ) {
-                db.execSQL(
-                    """
-                    CREATE TABLE IF NOT EXISTS `session_table` (
-                        `column_session_id` INTEGER PRIMARY KEY NOT NULL,
-                        `column_is_guest`   INTEGER NOT NULL DEFAULT 0
-                    )
-                    """.trimIndent(),
-                )
-            }
-        }
-    }
 }
