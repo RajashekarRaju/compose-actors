ac7ec5fcb39a2f86908d3a382eea31926112c8b3
Raj <rajashekarraju123@gmail.com>
2025-07-21 14:28:21 +0530

    Add screenshot tests for Forgot Password screen previews

---
+++ app/src/screenshotTest/kotlin/ForgotPasswordScreenPreviewTest.kt
@@ -0,0 +1,23 @@
+import androidx.compose.runtime.Composable
+import com.developersbreach.composeactors.annotations.PreviewLightDark
+import com.developersbreach.composeactors.ui.screens.forgotpassword.ForgotPasswordScreenConfirmResetPreview
+import com.developersbreach.composeactors.ui.screens.forgotpassword.ForgotPasswordScreenRequestResetPreview
+import com.developersbreach.composeactors.ui.screens.forgotpassword.ForgotPasswordScreenResetCompletedPreview
+
+@PreviewLightDark
+@Composable
+private fun ForgotPasswordScreenConfirmResetPreviewTest() {
+    ForgotPasswordScreenConfirmResetPreview()
+}
+
+@PreviewLightDark
+@Composable
+private fun ForgotPasswordScreenRequestResetPreviewTest() {
+    ForgotPasswordScreenRequestResetPreview()
+}
+
+@PreviewLightDark
+@Composable
+private fun ForgotPasswordScreenResetCompletedPreviewTest() {
+    ForgotPasswordScreenResetCompletedPreview()
+}
